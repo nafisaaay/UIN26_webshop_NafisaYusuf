6df841065d36599892c2de5f53b5a2adcd84f062
nafisaaay <nafisaay@hiof.no>
2026-01-19 08:18:06 +0100

    cart oppdatering
    "legg til i handlekurv" funker og det går an å fjerne fra kurven, og ferdig pris er oppdatert

--- LEGOdudes/shopscript.js
+++ LEGOdudes/shopscript.js
@@ -19,10 +19,68 @@ function fetchProducts() {
 
 fetchProducts()
 
+//Generer handlevogn 
+
+function showCart(){
+    //unike produkter 
+    let uniqueItems = new Set(cart) //lager nytt sett hvor hvert unikt set vises 
+    let uniqueArray = [...uniqueItems] //... sprer ut verdier inn i en array 
+    //oversikt over antall per produkt 
+    let cartItems = []
+    //går gjennom hele arrayen og filtrerer, og tar vare på lengden, og setter det inn i en ny array kalt cartItems, for å regne ut sum og vise antall
+    uniqueArray.map(item => {
+        cartItems.push({prodid: item, quantity: cart.filter(i => i === item).length})
+    })
+    //sjekker om dette blir rikitg 
+    //console.log(cartItems). --> brukes for å sjekke om ting funker i konsollen, fjernes når alt funker 
+
+    //gå gjennom cartItems for å lage HTML til handlekurv og renge totalpris 
+
+    let cartHTML = ""
+    let totalPrice = 0
+
+    cartItems.map(ci => {
+        //hente produktinformasjon 
+        //ci er produktid-en til arrayen vi hadde laget også sammenlignes det med produkt id-en
+        let product = products.find(i => i.prodid === ci.prodid)
+        //skrive HTML
+        cartHTML += `<tr>
+                        <td class="title">${product.title}</td>
+                        <td class="price">${product.price}</td>
+                        <td class="quantity">${ci.quantity}</td>
+                        <td class="delete"><button onClick="deleteFromCart(${product.prodid})">X</button></td>
+                    </tr>`
+
+        //summere totalprisen
+        totalPrice += Number(product.price) * Number(ci.quantity)
+    })
+
+    if(cart.length === 0){
+        cartHTML += "<tr><td>Ingen varer i handlevogn</td></tr>"
+    }
+
+    //henter id fra html og mater den inn i cart items
+    document.getElementById("cart-items").innerHTML = cartHTML
+    document.getElementById("total-price").innerHTML = totalPrice
+    document.getElementById("cart-quantity").innerHTML = cart.length
+}
+
+//slette fra handlevogn
+function deleteFromCart(prodid){
+    let deleteIndex = cart.indexOf(prodid)
+    if (deleteIndex > -1){
+        cart.splice(deleteIndex, 1)//sletter ett element fra den indeksen 
+    }
+    //oppdatere handlevogn utskrift 
+    showCart()
+}
+
 //Legg til i handlevogn
 function addToCart(prodid) {
     console.log("Legg til produkt med id: " + prodid)
     cart.push(prodid)
     console.log(cart)
-    document.getElementById("cart-quantity").innerHTML = cart.length
+
+    //oppdaterer handlevogn visning 
+    showCart()
 }
